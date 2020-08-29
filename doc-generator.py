6b58248ceb13e2035bda3908af2cc0dfe46bc242
Edvard Thörnros <edvard.thornros@gmail.com>
2020-08-29 23:00:21 +0200

    Add info for readers of the file

--- doc-generator.py
+++ doc-generator.py
@@ -1,25 +1,72 @@
 import ribs
 from inspect import ismodule, isfunction
+from dataclasses import dataclass
 
-docs = []
+sr_docs = []
 for name, val in ribs.__dict__.items():
     if name[0] == "_": continue
     if ismodule(val): continue
 
     if isfunction(val):
-        docs.append((name, val, val.__doc__))
+        sr_docs.append((name, val, [val.__doc__]))
     elif name.isupper():
-        docs.append((name, val, "constant"))
+        sr_docs.append((name, val, []))
 
 
-def gen_doc(name, docstring):
-    if docstring == "constant":
-        return f"<h2 id='{name}' class='constant'>name</h2>"
+def gen_doc(name, docstrings):
+    def format_doc(x):
+        return "<div class='docstr'>" + x.replace('\n\n', '<br>') + "</div>"
+
+    if docstrings:
+        out = "".join([format_doc(x) for x in docstrings])
+        return f"<div id='{name}' class='func'><h2>{name}</h2><p>{out}</p></div>"
     else:
-        docstring = docstring.replace('\n\n', '<br>')
-        return f"<div id='{name}' class='func'><h2>name</h2><p>{docstring}</p></div>"
+        return f"<h2 id='{name}' class='constant'>{name}</h2>"
+
+@dataclass
+class Docs:
+    name: str = "NO NAME"
+    docs = None
 
 
-docs = sorted(docs, key=lambda x: isfunction(x[1]))
-print(ribs.__dict__)
-print("\n".join([gen_doc(x[0], x[2]) for x in docs]))
+def parse_docs(filename):
+    with open(filename, "r") as f:
+        res = []
+        doc = Docs()
+        block = ""
+        for line in f:
+            if line.startswith("# "):
+                if block:
+                    doc.docs.append(block)
+                    block = ""
+                    res.append(doc)
+
+                doc = Docs()
+                doc.docs = []
+                doc.name = line[2:].strip()
+
+            elif line.startswith("## ex"):
+                if block:
+                    doc.docs.append(block)
+                    block = ""
+
+            else:
+                block += line
+        if block:
+            doc.docs.append(block)
+            block = ""
+            res.append(doc)
+        return res
+
+
+pg_docs = parse_docs("pygame.docs")
+sr_docs = sorted(sr_docs, key=lambda x: isfunction(x[1]))
+with open("docs.html", "w+") as f:
+    intro = "<!--- This file is auto generated, please do not edit --><h1>TODO INTRO</h1>"
+    toc = "<h1>TODO TOC</h1>"
+    f.write(intro)
+    f.write(toc)
+    f.write("<hr>")
+    f.write("".join([gen_doc(x.name, x.docs) for x in pg_docs]))
+    f.write("<hr>")
+    f.write("".join([gen_doc(x[0], x[2]) for x in sr_docs]))
