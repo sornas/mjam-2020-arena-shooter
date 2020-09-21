9a052144acc9dc6ff7d41e1ebea2044d50fcbbac
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-09-21 20:31:57 +0200

    Improve performance by quite a bit

--- ribs.py
+++ ribs.py
@@ -127,13 +127,24 @@ def clear_screen(color):
 # Text drawing
 #
 
+LOADED_FONTS = {}
 def draw_text(text, position, size=32, color=pg.Color(255, 255, 255), font=None):
     """
         Draw text at given position.
         The position is in pixels from the top left of the window.
         Optional arguments include size, color and font.
     """
-    font_obj = pg.font.SysFont(font, size)
+    global LOADED_FONTS
+
+    # Keep used fonts in memory.
+    # This is not a good solution if many different font sizes are used,
+    # but i cannot find a better way to do it...
+    if (font, size) not in LOADED_FONTS:
+        if len(LOADED_FONTS) > 100:
+            LOADED_FONTS.popitem()
+        LOADED_FONTS[(font, size)] = pg.font.SysFont(font, size)
+
+    font_obj = LOADED_FONTS[(font, size)]
 
     rendered_text = font_obj.render(text, True, color)
 
